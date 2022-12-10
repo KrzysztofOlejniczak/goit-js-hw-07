2875a496eb36a70ee4431246e60a1d8ae923058e
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-10 18:01:50 +0100

    task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+"use strict";
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((el) => {
+  const galleryElement = `<div class="gallery__item">
+    <a class="gallery__link" href="${el.original}">
+      <img
+        class="gallery__image"
+        src="${el.preview}"
+        data-source="${el.original}"
+        alt="${el.description}"
+      />
+    </a>
+  </div>`;
+  gallery.insertAdjacentHTML("beforeend", galleryElement);
+});
+
+gallery.addEventListener("click", (ev) => {
+  if (ev.target.nodeName !== "IMG") {
+    return;
+  }
+
+  ev.preventDefault();
+
+  const modal = basicLightbox.create(
+    `<img src="${ev.target.dataset.source}" width="800" height="600" />`
+  );
+  modal.show();
+  console.log("Opening modal window");
+
+  document.addEventListener("keydown", function escToClose(event) {
+    if (event.key === "Escape") {
+      modal.close();
+      document.removeEventListener("keydown", escToClose);
+      console.log("Closing modal window");
+    }
+  });
+});
