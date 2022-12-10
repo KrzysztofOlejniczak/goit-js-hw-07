d797ee23a8404be3bd9930a7a697d049cf37af94
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2022-12-10 19:56:35 +0100

    task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+"use strict";
+
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+galleryItems.forEach((el) => {
+  const galleryElement = `<a class="gallery__item" href="${el.original}">
+  <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+</a>`;
+  gallery.insertAdjacentHTML("beforeend", galleryElement);
+});
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+lightbox.on("show.simplelightbox", () => console.log("Opening modal window"));
+lightbox.on("close.simplelightbox", () => console.log("Closing modal window"));
